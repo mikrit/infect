6caec7385b72ca69782b13c8c0695709903d752c
mikrit <mikrit@gmail.com>
2017-06-29 22:30:11 +0300

    dev

--- application/views/data/index.php
+++ application/views/data/index.php
@@ -22,132 +22,72 @@
 <div class="tab-content">
 	<div id="panel1" class="tab-pane fade in active">
 		<h3>Инфекционная заболеваемость</h3>
-		<p>
-			<?=Form::open('data', array('class' => 'form-horizontal', 'method'=>'post', 'name' => 'infect'))?>
-				<div class="form-group">
-					<label for="inputEmail3" class="col-sm-2 control-label">Email</label>
-					<div class="col-sm-5">
-						<input type="email" class="form-control" id="inputEmail3" placeholder="Email">
-					</div>
-					<div class="col-sm-5">
-						<input type="email" class="form-control" id="inputEmail3" placeholder="Email">
-					</div>
-				</div>
-				<div class="form-group">
-					<label for="inputPassword3" class="col-sm-2 control-label">Password</label>
-					<div class="col-sm-10">
-						<input type="password" class="form-control" id="inputPassword3" placeholder="Password">
-					</div>
-				</div>
-				<div class="form-group">
-					<div class="col-sm-offset-2 col-sm-10">
-						<div class="checkbox">
-							<label>
-								<input type="checkbox"> Remember me
-							</label>
-						</div>
-					</div>
-				</div>
-				<div class="form-group">
-					<div class="col-sm-offset-2 col-sm-10">
-						<button type="submit" class="btn btn-default">Sign in</button>
-					</div>
-				</div>
-			<?=Form::close()?>
-		</p>
+        <?=Form::open('data', array('method'=>'post', 'name' => 'infect'))?>
+            <table class="table">
+                <tr>
+                    <th>
+                        Инфекционная и паразитарная заболеваемость
+                    </th>
+                    <th>
+                        Абс.
+                    </th>
+                    <th>
+                        На 100 тыс. населения
+                    </th>
+                </tr>
+                <tr>
+                    <td>
+                        Брюшной тиф
+                    </td>
+                    <td>
+                        <?=Form::input('bru_tif', '',array("class" => "form-control", "name" => "bru_tif", "type" => "text", "autofocus" => ""))?>
+                    </td>
+                    <td>
+                        <?=Form::input('t_bru_tif', '',array("class" => "form-control", "name" => "t_bru_tif", "type" => "text"))?>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Паратифы
+                    </td>
+                    <td>
+                        <?=Form::input('paratif', '',array("class" => "form-control", "name" => "paratif", "type" => "text"))?>
+                    </td>
+                    <td>
+                        <?=Form::input('t_paratif', '',array("class" => "form-control", "name" => "t_paratif", "type" => "text"))?>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        паратиф С
+                    </td>
+                    <td>
+                        <?=Form::input('paratif_c', '',array("class" => "form-control", "name" => "paratif_c", "type" => "text"))?>
+                    </td>
+                    <td>
+                        <?=Form::input('t_paratif_c', '',array("class" => "form-control", "name" => "t_paratif_c", "type" => "text"))?>
+                    </td>
+                </tr>
+            </table>
+        <?=Form::close()?>
 	</div>
 	<div id="panel2" class="tab-pane fade">
 		<h3>Инф служба </h3>
-		<p>
-			<?=Form::open('data', array('method'=>'post', 'name' => 'inf_secur'))?>
-				<label for="exampleSelect1">Обслуживаемое население:</label>
-				<div class="form-group">
-					<?=Form::input('adults', '',array("class" => "form-control", "placeholder" => "взрослые", "name" => "adult", "type" => "text", "autofocus" => ""))?>
-				</div>
-				<div class="form-group">
-					<?=Form::input('children', '',array("class" => "form-control", "placeholder" => "дети (0-17)", "name" => "children", "type" => "text"))?>
-				</div>
-				<label for="exampleSelect1">Количество случаев инфекционных заболеваний по данным отчетной формы №2:</label>
-				<div class="form-group">
-					<?=Form::input('inf_adults', '',array("class" => "form-control", "placeholder" => "у взрослых (с 18 лет)", "name" => "inf_adults", "type" => "text", "autofocus" => ""))?>
-				</div>
-				<div class="form-group">
-					<?=Form::input('inf_children', '',array("class" => "form-control", "placeholder" => "у детей (0-17)", "name" => "inf_children", "type" => "text"))?>
-				</div>
-				<?=Form::hidden('action', 'inf_secur')?>
-				<?=Form::submit('button', 'Сохранить', array("class" => "btn btn-primary", "type" => "button"))?>
-			<?=Form::close()?>
-		</p>
 	</div>
 	<div id="panel3" class="tab-pane fade">
 		<h3>Стац помощь</h3>
-		<p>
-			<?=Form::open('data', array('method'=>'post', 'name' => 'stac'))?>
-				<div class="form-group">
-					<?=Form::input('', '',array("class" => "form-control", "placeholder" => "", "name" => "", "type" => "text", "autofocus" => ""))?>
-				</div>
-				<div class="form-group">
-					<?=Form::input('', '',array("class" => "form-control", "placeholder" => "", "name" => "", "type" => "text"))?>
-				</div>
-				<?=Form::submit('button', 'Сохранить', array("class" => "btn btn-primary", "type" => "button"))?>
-			<?=Form::close()?>
-		</p>
 	</div>
 	<div id="panel4" class="tab-pane fade">
 		<h3>СПИД-центры</h3>
-		<p>
-			<?=Form::open('data', array('method'=>'post', 'name' => 'spid'))?>
-				<div class="form-group">
-					<?=Form::input('', '',array("class" => "form-control", "placeholder" => "", "name" => "", "type" => "text", "autofocus" => ""))?>
-				</div>
-				<div class="form-group">
-					<?=Form::input('', '',array("class" => "form-control", "placeholder" => "", "name" => "", "type" => "text"))?>
-				</div>
-				<?=Form::submit('button', 'Сохранить', array("class" => "btn btn-primary", "type" => "button"))?>
-			<?=Form::close()?>
-		</p>
 	</div>
 	<div id="panel5" class="tab-pane fade">
 		<h3>Амбулат помощь</h3>
-		<p>
-			<?=Form::open('data', array('method'=>'post', 'name' => 'ambulat'))?>
-				<div class="form-group">
-					<?=Form::input('', '',array("class" => "form-control", "placeholder" => "", "name" => "", "type" => "text", "autofocus" => ""))?>
-				</div>
-				<div class="form-group">
-					<?=Form::input('', '',array("class" => "form-control", "placeholder" => "", "name" => "", "type" => "text"))?>
-				</div>
-				<?=Form::submit('button', 'Сохранить', array("class" => "btn btn-primary", "type" => "button"))?>
-			<?=Form::close()?>
-		</p>
 	</div>
 	<div id="panel6" class="tab-pane fade">
 		<h3>КДЦ</h3>
-		<p>
-			<?=Form::open('data', array('method'=>'post', 'name' => 'kdc'))?>
-				<div class="form-group">
-					<?=Form::input('', '',array("class" => "form-control", "placeholder" => "", "name" => "", "type" => "text", "autofocus" => ""))?>
-				</div>
-				<div class="form-group">
-					<?=Form::input('', '',array("class" => "form-control", "placeholder" => "", "name" => "", "type" => "text"))?>
-				</div>
-				<?=Form::submit('button', 'Сохранить', array("class" => "btn btn-primary", "type" => "button"))?>
-			<?=Form::close()?>
-		</p>
 	</div>
 	<div id="panel7" class="tab-pane fade">
 		<h3>Вирусные гепатиты</h3>
-		<p>
-			<?=Form::open('data', array('method'=>'post', 'name' => 'vir_gepatit'))?>
-				<div class="form-group">
-					<?=Form::input('', '',array("class" => "form-control", "placeholder" => "", "name" => "", "type" => "text", "autofocus" => ""))?>
-				</div>
-				<div class="form-group">
-					<?=Form::input('', '',array("class" => "form-control", "placeholder" => "", "name" => "", "type" => "text"))?>
-				</div>
-				<?=Form::submit('button', 'Сохранить', array("class" => "btn btn-primary", "type" => "button"))?>
-			<?=Form::close()?>
-		</p>
 	</div>
 </div>
 
