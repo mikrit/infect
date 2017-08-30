e65cc0f703bdc0a8c1ce50a6c1ec95edc82b483a
IsaevEA <IsaevEA@materiamedica.ru>
2017-08-30 13:14:21 +0300

    reneme model

---
+++ application/classes/Model/Datainfect.php
@@ -0,0 +1,15 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+class Model_Datainfect extends ORM
+{
+	protected $_belongs_to = array(
+		'district'	=> array(
+			'model'			=> 'district',
+			'foreign_key'	=> 'district_id',
+		),
+		'subject'	=> array(
+			'model'			=> 'subject',
+			'foreign_key'	=> 'subject_id',
+		)
+	);
+}
